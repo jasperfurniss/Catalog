07c7efd467b1c1dbb2c70351dd62e085cafd5de7
Jasper Furniss <jfurniss@worthwhile.com>
2016-02-17 15:43:02 -0500

    Updated Project with Additional Views & Templates

--- course/views.py
+++ course/views.py
@@ -1,4 +1,4 @@
-from django.views.generic import UpdateView
+from django.views.generic import ListView, DetailView, UpdateView
 from django.shortcuts import get_object_or_404
 from django.core.urlresolvers import reverse_lazy
 
@@ -10,7 +10,7 @@ class Create(UpdateView):
     model = Course
     template_name = 'course/course_form.html'
     form_class = CourseForm
-    success_url = reverse_lazy('course')
+    success_url = reverse_lazy('course_list')
 
     def get_object(self, queryset=None):
         pk = self.kwargs.get('pk', None)
@@ -19,3 +19,11 @@ class Create(UpdateView):
         else:
             return Course()
 
+class CourseList(ListView):
+    model = Course
+    template_name = 'course/course_list.html'
+
+
+class CourseDetail(DetailView):
+    model = Course
+    template_name = 'course/course_detail.html'
