71a1cc6dc2f56565dcc593f78af15b794c2f418d
Jasper Furniss <jfurniss@worthwhile.com>
2016-02-17 09:00:10 -0500

    Adding Migrations

--- course/models.py
+++ course/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django import forms
 
 from filer.fields.image import FilerImageField
 
@@ -13,8 +14,8 @@ class Course(models.Model):
     course_title = models.CharField(max_length=255, null=True, blank=True)
     description = models.CharField(max_length=255, null=True, blank=True)
     instructor = models.CharField(max_length=255, null=True, blank=True)
-    course_duration = models.CharField(choices=DURATION)
-    art = FilerImageField()
+    course_duration = forms.ChoiceField(choices=DURATION, widget=forms.RadioSelect)
+    art = FilerImageField(null=True, blank=True, related_name='art_image_collection')
 
     def __unicode__(self):
         return u'{}'.format(self.name)
